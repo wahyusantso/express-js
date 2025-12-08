7e17c276027499db367175babf190d7fd1c47ef5
wahyusantso <wahyusantozo36@gmail.com>
2025-12-08 11:43:39 +0700

    127: Basic routing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+//simple routing
+app.get('/', (req, res) => {
+    res.send('First code express js.');
+});
+
 app.listen(3000, () => {
     console.info('Server started on port 3000');
 });
