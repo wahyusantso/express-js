a85f97d9bf035d8aecdfa8f18c6c07944bf1491e
wahyusantso <wahyusantozo36@gmail.com>
2025-12-13 10:42:13 +0700

    151: File Upload

--- test/request-body.test.js
+++ test/request-body.test.js
@@ -1,9 +1,11 @@
 import express, { json } from 'express';
 import request from 'supertest';
+import expressFileUpload from 'express-fileupload';
 
 const app = express();
 app.use(express.json()); //menggunakan built-in middleware untuk konversi data body ke json.
 app.use(express.urlencoded({extended: false})); //konversi ke form, set ke false supaya tidak membaca data dari query parameter
+app.use(expressFileUpload()); //tambahkan lib file upload
 
 //simple routing
 app.post('/json', (req, res) => {
@@ -20,6 +22,14 @@ app.post('/form', (req, res) => {
     res.json({
         name: `Hello ${name}`
     });
+});
+
+app.post('/file', async (req, res) => {
+    //untuk mendapatkan file yang dikirim. gunakan files bukan body dan pastikan key sesuai dengan yang di kirim (article).
+    const file = req.files.article;
+    await file.mv(__dirname + '/upload/' + file.name); //set lokasi file disimpan. menggunakan await karena return promise.
+
+    res.send(`File ${file.name} Uploaded by user ${req.body.name}`);
 });
 
 test('Test Request Json', async () => {
@@ -40,4 +50,14 @@ test('Test Request Form', async () => {
     expect(response.body).toEqual({
         name: 'Hello Royes'
     });
+});
+
+test('Test Request File', async () => {
+    const response = await request(app)
+        .post('/file')
+        .set('Content-Type', 'multipart/form-data') //header untuk upload file
+        .field('name', 'Andrew') //sisipkan data ke body
+        .attach('article', __dirname + '/contoh.txt');//file yang dikirim
+        
+    expect(response.text).toBe('File contoh.txt Uploaded by user Andrew');
 });
